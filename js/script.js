ba73015d247a464f02d39e6533b3c09f52d8b808
Julia Almeida <julia.almeida.veleiros@gmail.com>
2024-11-13 00:44:48 -0300

    update2!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+// Mudança de tema de dark para light
+const toggleTheme = document.getElementById("toggleTheme"); /* chama o elemento toggleTheme */
+const rootHtml = document.documentElement;
+
+// Função para Alterar o Tema
+
+function changeTheme () {
+  const currentTheme = rootHtml.getAttribute("data-theme");
+
+  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+
+  toggleTheme.classList.toggle("bi-sun");
+  toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+// Fim da Função de Mudar Tema
+
+
 /*-------------------------------Menu-------------------------------------- */ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
@@ -40,3 +59,5 @@ sr.reveal('.home-text, .about-img, .skills-subtitle, .skills-text, .skills-title
 sr.reveal('.home-img, .about-subtitle, .about-text, .skills-img',{delay: 400}); 
 sr.reveal('.icon, .img',{ interval: 200}); 
 sr.reveal('.skills-data, .work-img, .contact-input',{interval: 200}); 
+
+
